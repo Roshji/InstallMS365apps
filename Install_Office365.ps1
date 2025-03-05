a701915359385cb3e809464721469a672c641e05
Roshji <remco.vandiermen@outlook.com>
2025-03-05 10:57:52 +0100

    Update Install_Office365.ps1
    Updated Script for Detection of old versions of MS365 apps

--- Install_Office365.ps1
+++ Install_Office365.ps1
@@ -1,7 +1,11 @@
-<#
-###################
-Authors: M van Rijn - Prodicom 
-Remco van Diermen - RvD IT Consulting
+<# ###################
+Author: M van Rijn - Prodicom
+
+Version 1.1 - 2025-03-03 - Remco van Diermen (RvD IT Consulting)
+
+### CHANGELOG ###
+- Updated the Download URL Link
+- Added Version checking in section Get-OfficeSource, if an old version of MS365 apps is installed it will be removed first. 
 
 # --- UNINSTALL ---
 # Some vendors pre-install [Productname], such as Microsoft Surface devices
@@ -12,38 +16,36 @@ Remco van Diermen - RvD IT Consulting
 # --- INSTALL ---
 # Specify languages in "Install_[Productname].xml" for installation
 
-1. Check if [Productname] is installed
+1. Check if [Productname] is installed and the version isn't older than x
 --> YES
 Remove languages specified in Uninstall_[Productname]_Languages.xml, end logging and script
 --> NO
 - Create download folder for Officedeploymenttool
 - Download latest version of the Officedeploymenttool
 - Unpack Officedeploymenttool
-- Download [Productname] via Officedeploymenttool and Install_[Productname].xml to the (package) $Global:DownloadAppFolder
+- Download [Productname] via Officedeploymenttool and Install_[Productname].xml to the (package) $DownloadAppFolder
 - Install [Productname] via Officedeploymenttool and Install_[Productname].xml
 
 The Officedeploymenttool folder will remain on the device for uninstall
-####################
-#>
+#################### #>
 
 # Vars
 $Date = (Get-Date).tostring("yyyy-MM-dd HHmm")
-$Global:ProductName = "Office365"
-$Global:ExtractAppFolder = "Officedeploymenttool"
-$Global:ODTExe = "officedeploymenttool.exe"
-$Global:DownloadAppFolder = "C:\ProgramData\Intune\Packages\$($Global:ProductName)"
-$LogFile = "C:\Programdata\Microsoft\IntuneManagementExtension\Logs\Logs_$($Global:ProductName)_install_$($Date).log"
-$Global:installFolder = "$PSScriptRoot"
+$ProductName = "Office365"
+$ExtractAppFolder = "Officedeploymenttool"
+$ODTExe = "officedeploymenttool.exe"
+$DownloadAppFolder = "C:\ProgramData\Intune\Packages\$($ProductName)"
+$LogFile = "C:\Programdata\Microsoft\IntuneManagementExtension\Logs\Logs_$($ProductName)_install_$($Date).log"
+$InstallFolder = "$PSScriptRoot"
 $Office365Path = "C:\Program Files\Common Files\Microsoft Shared\ClickToRun\OfficeClickToRun.exe"
+$targetVersion = '16.0.18429.20158'
+#$Url = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117"
 # Link changed on Dec 6 2024
 $Url = "https://www.microsoft.com/en-us/download/details.aspx?id=49117"
-#$Url = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117"
 $Response = Invoke-WebRequest -UseBasicParsing -Uri $url -ErrorAction SilentlyContinue
-$Global:SetupEXEFile = "$Global:DownloadAppFolder\$Global:ExtractAppFolder\Setup.exe"
-$Global:InstallXML = "Install_$Global:ProductName.xml"
-$UnInstallXML = "Uninstall_$($Global:ProductName)_Languages.xml"
-
-
+$SetupEXEFile = "$DownloadAppFolder\$ExtractAppFolder\Setup.exe"
+$InstallXML = "Install_$ProductName.xml"
+$UnInstallXML = "Uninstall_$($ProductName)_Languages.xml"
 
 # Start Logging
 Start-Transcript -Path $LogFile
@@ -55,67 +57,74 @@ Function Get-OfficeSource {
     # FOLDER CREATION SECTION #
     ###########################
     # Check if folder exist else create
-    If (!(Test-Path $Global:DownloadAppFolder)) {
+    If (!(Test-Path $DownloadAppFolder)) {
         # Create directory
-        Write-Host "Create ODT package folder: $Global:DownloadAppFolder"
-        New-item -path "$Global:DownloadAppFolder" -ItemType "directory"
+        Write-Host "Create ODT package folder: $DownloadAppFolder"
+        New-item -path "$DownloadAppFolder" -ItemType "directory"
     }
 
     ####################
     # DOWNLOAD SECTION #
     ####################
     # Get Download URL of latest Office Deployment Tool (ODT)
-  # $ODTUri = $response.links | Where-Object {$_.outerHTML -like "*click here to download manually*"}
+    #$ODTUri = $response.links | Where-Object {$_.outerHTML -like "*click here to download manually*"}
+    #$UrlCurrentVerODT = $ODTUri.href
     $ODTUri = $response.Links | Where-Object { $_.href -match "https://download\.microsoft\.com/download/.*/officedeploymenttool_.*\.exe" }
     $UrlCurrentVerODT = $ODTUri.href
-  #  $UrlCurrentVerODT = $ODTUri.href
     # Download latest Office Deployment Tool (ODT)
     Write-Host "Downloading latest version of Office Deployment Tool (ODT)."
-    Invoke-WebRequest -UseBasicParsing -Uri $UrlCurrentVerODT -OutFile $Global:DownloadAppFolder\$Global:ODTExe
+    Invoke-WebRequest -UseBasicParsing -Uri $UrlCurrentVerODT -OutFile $DownloadAppFolder\$ODTExe
     # Get file version
     Write-Host "Get fileversion Office Deployment Tool (ODT)."
-    $Version = (Get-Command $Global:DownloadAppFolder\$Global:ODTExe).FileVersionInfo.FileVersion
+    $Version = (Get-Command $DownloadAppFolder\$ODTExe).FileVersionInfo.FileVersion
     Write-Host "Fileversion Office Deployment Tool (ODT): " $Version
     # Unpack ODT File
     Write-Host "Unpacking file ..."
-    $arguments1 = "/quiet /extract:$Global:DownloadAppFolder\$Global:ExtractAppFolder"
-    Start-Process -Wait -FilePath "$Global:DownloadAppFolder\$Global:ODTExe" -ArgumentList $arguments1
+    $arguments1 = "/quiet /extract:$DownloadAppFolder\$ExtractAppFolder"
+    Start-Process -Wait -FilePath "$DownloadAppFolder\$ODTExe" -ArgumentList $arguments1
     Start-sleep -s 5
     # Download
-    $arguments1 = "/download $Global:installFolder\$Global:InstallXML" 
-    Write-Host "Start download via $Global:SetupEXEFile  $arguments1..."
-    Start-Process -Wait -FilePath "$Global:SetupEXEFile" -ArgumentList $arguments1
+    $arguments1 = "/download $installFolder\$InstallXML" 
+    Write-Host "Start download via $SetupEXEFile  $arguments1..."
+    Start-Process -Wait -FilePath "$SetupEXEFile" -ArgumentList $arguments1
 }
 
-# Check if [Productname] (64 bits) is already installed
+# Check if [Productname] (64 bits) is already installed and if version matche
 
-If (Test-Path $Office365Path) {
+$fileVersion = (Get-Item 'C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeClickToRun.exe').VersionInfo.FileVersion
+
+If ((Test-Path $Office365Path) -and ([version]$fileVersion -ge [version]$targetVersion)) {
     #####################
     # UNINSTALL SECTION #
     #####################
-    Write-Host "$Global:ProductName installation found..."
+    Write-Host "$ProductName installation found..."
     # Getting installed components   
-    Write-host "Installed $Global:ProductName components..." 
+    Write-host "Installed $ProductName components..." 
     Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\O365ProPlusRetail* | Select-Object DisplayName -expandproperty Displayname
 
     # Check if original Setup files are present on the system, if not download ODT
-    If (!(Test-Path $Global:SetupEXEFile)) {Get-OfficeSource} # Call Function
+    If (!(Test-Path $SetupEXEFile)) {Get-OfficeSource} # Call Function
 
     # Specify all languages to uninstall in file "Uninstall_[Productname]_Languages.xml"
     # If the languages to uninstall need to be defined first, run the commands above (Get-ItemProperty..) on an identical device and update the file "Uninstall_[Productname]_Languages.xml"
     # Uninstall languages
-    Write-Host "Uninstall $Global:ProductName languages...based on XML $Global:installFolder\$UnInstallXML"
-    $arguments1 = "/configure $Global:installFolder\$UnInstallXML" 
-    Start-Process -Wait -FilePath "$Global:SetupEXEFile" -ArgumentList $arguments1
+    Write-Host "Uninstall $ProductName languages...based on XML $installFolder\$UnInstallXML"
+    $arguments1 = "/configure $installFolder\$UnInstallXML" 
+    Start-Process -Wait -FilePath "$SetupEXEFile" -ArgumentList $arguments1
     
 } Else {
-    Write-Host "No $Global:ProductName installation found. Download and install $Global:ProductName..."
+    Write-Host "No $ProductName installation or older version found. Download and install $ProductName..."
+    Write-Host "Currentversion is $fileVersion"
     Get-OfficeSource # Call Function download ODT
 
     # Install
-    Write-Host "Installing $Global:ProductName..."
-    $arguments1 = "/configure $Global:installFolder\$Global:InstallXML" 
-    Start-Process -Wait -FilePath "$Global:SetupEXEFile" -ArgumentList $arguments1
+    Write-Host "Installing $ProductName..."
+    $arguments1 = "/configure $installFolder\$InstallXML" 
+    Start-Process -Wait -FilePath "$SetupEXEFile" -ArgumentList $arguments1
+
+    Write-Host "Uninstall $ProductName languages...based on XML $installFolder\$UnInstallXML"
+    $arguments1 = "/configure $installFolder\$UnInstallXML" 
+    Start-Process -Wait -FilePath "$SetupEXEFile" -ArgumentList $arguments1
 }
 
 ###################
@@ -124,7 +133,7 @@ If (Test-Path $Office365Path) {
 Start-sleep -s 5
 # Cleanup ODT file. Folder with officedeploymenttool and software will remain for uninstall. Change if needed
 Write-Host "Cleanup ODT download file"
-Remove-Item "$Global:DownloadAppFolder\$Global:ODTExe"
+Remove-Item "$DownloadAppFolder\$ODTExe"
 
 Start-sleep -s 20
 
